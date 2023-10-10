50638a52836228680ec81171260b3699fa78e5a1
Ellian Campos <ellian.campos12@gmail.com>
2023-10-10 16:36:01 -0600

    COMMIT

--- PRESENTACION/FrmCrearTarea.cs
+++ PRESENTACION/FrmCrearTarea.cs
@@ -12,83 +12,73 @@ using ENTIDADES;
 
 namespace PRESENTACION
 {
-	public partial class FrmCrearTarea : Form
-	{
-		LogicaTareas logicaTareas = new LogicaTareas();
-		LogicaEstados logicaEstados = new LogicaEstados();
+    public partial class FrmCrearTarea : Form
+    {
+        LogicaTareas logicaTareas = new LogicaTareas();
+        LogicaEstados logicaEstados = new LogicaEstados();
+        LogicaCategorias logicaCategorias = new LogicaCategorias();
 
-		public FrmCrearTarea()
-		{
-			InitializeComponent();
-		}
+        public FrmCrearTarea()
+        {
+            InitializeComponent();
+        }
 
-		// Validar campos 
-		public bool ValidarCampos()
-		{
-			bool errorFlag = false;
-			errorProvider1.Clear();
+        // Validar campos 
+        public bool ValidarCampos()
+        {
+            bool errorFlag = false;
+            errorProvider1.Clear();
 
-			if (txtTitulo.Text.Equals(""))
-			{
-				errorProvider1.SetError(txtTitulo, "Campo requerido");
-				errorFlag = true;
-			}
+            if (txtTitulo.Text.Equals(""))
+            {
+                errorProvider1.SetError(txtTitulo, "Campo requerido");
+                errorFlag = true;
+            }
 
-			if (errorFlag) { return true; } else { return false; }
-		}
+            if (errorFlag) { return true; } else { return false; }
+        }
 
-		// Limpiar campos 
-		public void LimpiarCampos()
-		{
-			txtTitulo.Clear();
-			cbEstado.Text = "";
-			cbCategoria.Text = "";
-			txtApuntes.Text = "";
-		}
+        // Limpiar campos 
+        public void LimpiarCampos()
+        {
+            txtTitulo.Clear();
+            cbEstado.Text = "";
+            cbCategoria.Text = "";
+            txtApuntes.Text = "";
+        }
 
-		private void FrmCrearTarea_Load(object sender, EventArgs e)
-		{
-			MessageBox.Show("hola");
-			//cbEstado.DataSource = logicaEstados.ObtenerEstados();
-			foreach (var item in logicaEstados.ObtenerEstados())
-			{
-				MessageBox.Show("ahhh" + item);
-			}
-		}
+        private void FrmCrearTarea_Load(object sender, EventArgs e)
+        {
+            foreach (Estado estado in logicaEstados.ObtenerEstados())
+                cbEstado.Items.Add(estado.Nombre);
 
-		private void btnCrear_Click(object sender, EventArgs e)
-		{
-			// Validar campos
-			if (ValidarCampos()) return;
+            foreach (Categoria categoria in logicaCategorias.ObtenerCategorias())
+                cbCategoria.Items.Add(categoria.Nombre);
+        }
 
-			// Crear una nueva tarea
-			if (!logicaTareas.CrearTarea(new Tarea(txtTitulo.Text, dtpFecha.Value,
-				cbEstado.Text, cbCategoria.Text, txtApuntes.Text)))
-			{
-				MessageBox.Show("No se ha podido crear la tarea", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				return;
-			}
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            // Validar campos
+            if (ValidarCampos()) return;
 
-			LimpiarCampos();
-			this.Close();
-		}
+            // Crear una nueva tarea
+            if (!logicaTareas.CrearTarea(new Tarea(txtTitulo.Text, dtpFecha.Value,
+                cbEstado.Text, cbCategoria.Text, txtApuntes.Text)))
+            {
+                MessageBox.Show("No se ha podido crear la tarea", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
 
-		private void FrmCrearTarea_Click(object sender, EventArgs e)
-		{
-			foreach (var item in logicaEstados.ObtenerEstados())
-			{
-				MessageBox.Show("ahhh" + item);
-			}
-		}
+            LimpiarCampos();
+            this.Close();
+        }
 
-		private void label1_Click(object sender, EventArgs e)
-		{
-			////MessageBox.Show("hola");
-			////List<string> list = logicaEstados.ObtenerEstados();
-			////foreach (var item in list)
-			////{
-			////	MessageBox.Show("ahhh" + item);
-			////}
-		}
-	}
+        private void FrmCrearTarea_Click(object sender, EventArgs e)
+        {
+            foreach (var item in logicaEstados.ObtenerEstados())
+            {
+                MessageBox.Show("ahhh" + item);
+            }
+        }
+    }
 }
